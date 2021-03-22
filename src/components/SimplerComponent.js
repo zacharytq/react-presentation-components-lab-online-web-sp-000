07947c91d015d4522fcc33fe525a0e56345977f6
zacharytq <zacharytq@gmail.com>
2021-03-22 10:30:02 -0400

    commit tree 2f947b0cfe0e659778c4a3b14a926ecc3b35f1e6

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,10 @@
 // Code SimplerComponent Here
+import React from 'react';
+
+const SimplerComponent = props => {
+    return(
+        <div onClick={props.handleClick}>I am just happy</div>
+    )
+}
+
+export default SimplerComponent;
